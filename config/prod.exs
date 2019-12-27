652571c74eeea4504d2eee35b043d5e05f14c171
Eric McKnight <ericmck@gmail.com>
2019-12-27 18:50:06 -0500

    Fixed typo.

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :hostaway, HostawayWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "gentle-basin-37816.herokuapp.com", port: 443}],
+  url: [scheme: "https", host: "gentle-basin-37816.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forward_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
