d0d3226a3a8a2339196f1ba55d19662a2f0e29bd
Eric McKnight <ericmck@gmail.com>
2019-12-27 19:04:53 -0500

    Force ssl.

--- config/prod.exs
+++ config/prod.exs
@@ -12,8 +12,9 @@ use Mix.Config
 config :hostaway, HostawayWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "gentle-basin-37816.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forward_proto]],
+  force_ssl: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
+  # force_ssl: [rewrite_on: [:x_forward_proto]],
 
 # Do not print debug messages in production
 config :logger, level: :info
